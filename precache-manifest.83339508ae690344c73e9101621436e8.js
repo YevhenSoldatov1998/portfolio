c0904a6bb8b0f516a59b15f236e8cdf682a40431
YevhenSoldatov1998 <arr.ess1998@gmail.com>
2019-12-18 21:40:39 +0300

    Updates

---
+++ precache-manifest.83339508ae690344c73e9101621436e8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f013e0210aa8466848aef857e20b31",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cf4ce44a801d3e244f3b",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "327faa5666a2fbb7a9c8",
+    "url": "/portfolio/static/css/main.1062ac74.chunk.css"
+  },
+  {
+    "revision": "cf4ce44a801d3e244f3b",
+    "url": "/portfolio/static/js/2.0b0bf6ee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.0b0bf6ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "327faa5666a2fbb7a9c8",
+    "url": "/portfolio/static/js/main.e31a332e.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "d1c0abc02dbfa002e52f67c965484b67",
+    "url": "/portfolio/static/media/mountapp.d1c0abc0.png"
+  },
+  {
+    "revision": "50a85d5ba936d975622ea382333d46a0",
+    "url": "/portfolio/static/media/mountdown.50a85d5b.png"
+  },
+  {
+    "revision": "24f18e4603ea9603c7c4c7c8cdab35d2",
+    "url": "/portfolio/static/media/samurai.24f18e46.png"
+  },
+  {
+    "revision": "e4e5624ce8aa16ca33518ff088926693",
+    "url": "/portfolio/static/media/sky.e4e5624c.jpg"
+  }
+]);
