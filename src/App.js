b8ab762742aad2db902119be28b17ee31317e9a3
YevhenSoldatov1998 <arr.ess1998@gmail.com>
2019-12-21 19:05:18 +0300

    sun style modified

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from "./components/header/Header";
 import {BrowserRouter} from "react-router-dom";
 
 const App = (props) => {
+    debugger
     return (
         <BrowserRouter>
             <div className={style.appWrap}>
